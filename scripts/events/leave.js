b871cda9e67f7fa63f7930981d03467b81e79c71
Usernewbot <iam.jubayer.999@gmail.com>
2024-10-16 00:34:13 +0600

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -13,14 +13,14 @@ module.exports.run = async function({ api, event, Users, Threads }) {
 	const { threadID } = event;
 	const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
 	const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
-	const type = (event.author == event.logMessageData.leftParticipantFbId) ? "লিভ নেউয়ার জন্য ধন্যবাদ 🤢" : "Kicked by Administrator";
+	const type = (event.author == event.logMessageData.leftParticipantFbId) ? "༊═══❥༊᭄জোর করে কাউকে আটকে রাখা যায়না༊᭄❥┼─༊🥀😍💖 ღ●───༊,༊᭄,যে থাকার জন্য আসে,”❥┼─༊🌺😌💘 ღ●───༊”༊᭄সে কখনো যাওয়ার কথা বলে না।༊᭄ ●───🥰🥀🌺 🌸!!-!!🌸": "আবার অ্যাড দিবো🙂🫰";
 	const path = join(__dirname, "nayan", "leaveGif");
 	const gifPath = join(path, `l.gif`);
 	var msg, formPush
 
 	if (existsSync(path)) mkdirSync(path, { recursive: true });
 
-	(typeof data.customLeave == "undefined") ? msg = "তুই {name} গ্রুপে থাকার যোগ্য না আবাল .\n\n{type} " : msg = data.customLeave;
+	(typeof data.customLeave == "undefined") ? msg = "ইস_{name} 👾 .\n\n{type} " : msg = data.customLeave;
 	msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type);
 
 	if (existsSync(gifPath)) formPush = { body: msg, attachment: createReadStream(gifPath) }
