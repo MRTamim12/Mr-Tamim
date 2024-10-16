e6f78a7756e096ffdcfbe18f65443ed06221288d
Usernewbot <iam.jubayer.999@gmail.com>
2024-10-16 07:23:34 +0600

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -1,30 +1,33 @@
 module.exports.config = {
-	name: "leave",
-	eventType: ["log:unsubscribe"],
-	version: "1.0.0",
-	credits: "Nayan",
-	description: "notify leave.",
+  name: "leave",
+  eventType: ["log:unsubscribe"],
+  version: "1.0.0",
+  credits: "Nayan",
+  description: "notify leave.",
 };
 
 module.exports.run = async function({ api, event, Users, Threads }) {
-	if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
-	const { createReadStream, existsSync, mkdirSync } = global.nodemodule["fs-extra"];
-	const { join } =  global.nodemodule["path"];
-	const { threadID } = event;
-	const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
-	const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
-	const type = (event.author == event.logMessageData.leftParticipantFbId) ? "༊═══❥༊᭄জোর করে কাউকে আটকে রাখা যায়না༊᭄❥┼─༊🥀😍💖 ღ●───༊,༊᭄,যে থাকার জন্য আসে,”❥┼─༊🌺😌💘 ღ●───༊”༊᭄সে কখনো যাওয়ার কথা বলে না।༊᭄ ●───🥰🥀🌺 🌸!!-!!🌸": "আবার অ্যাড দিবো🙂🫰";
-	const path = join(__dirname, "nayan", "leaveGif");
-	const gifPath = join(path, `l.gif`);
-	var msg, formPush
+  if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
+  const { createReadStream, existsSync, mkdirSync } = global.nodemodule["fs-extra"];
+  const { join } = global.nodemodule["path"];
+  const { threadID } = event;
+  const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
+  const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
+  const type = (event.author == event.logMessageData.leftParticipantFbId) ? "তোর কতো বড় সাহস আমি জুবায়ের bot থাকতে তুই লিফট নিবি? থাপ্পর চিনো তুই☹️😡 🥱দারা তোরে আবার এট দিবো 🤨" : "বাল পাকনামির কারণে কিক খাইলো🤧";
+  const path = join(__dirname, "Nayan", "leaveGif");
+  const gifPath = join(path, `by.gif`);
+  let msg, formPush;
 
-	if (existsSync(path)) mkdirSync(path, { recursive: true });
+  if (existsSync(path)) mkdirSync(path, { recursive: true });
 
-	(typeof data.customLeave == "undefined") ? msg = "ইস_{name} 👾 .\n\n{type} " : msg = data.customLeave;
-	msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type);
+  typeof data.customLeave === "undefined" ? (msg = "ইস {name} {type} 🤖.") : (msg = data.customLeave);
+  msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type);
 
-	if (existsSync(gifPath)) formPush = { body: msg, attachment: createReadStream(gifPath) }
-	else formPush = { body: msg }
-	
-	return api.sendMessage(formPush, threadID);
-}
+  if (existsSync(gifPath)) {
+    formPush = { body: msg, attachment: createReadStream(gifPath) };
+  } else {
+    formPush = { body: msg };
+  }
+
+  return api.sendMessage(formPush, threadID);
+};
